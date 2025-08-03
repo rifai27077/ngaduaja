f9eb653d8757d6df2f77a9efc3eff5a0745c29eb
rifai27077 <ahmadrifai121209@gmail.com>
2025-08-03 21:45:14 +0700

    add laporan, regis, login

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\LaporanController;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('home');
-});
-Route::get('/login', function () {
-    return view('auth/login');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
+
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/lapor', function () {
+        return view('laporan.form');
+    })->name('laporan.form');
+
+    Route::post('/lapor', [LaporanController::class, 'store'])->name('laporan.store');
 });
